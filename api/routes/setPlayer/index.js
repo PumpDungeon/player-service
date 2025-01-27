4b395a9bfe45fac148da20da7df1f21e032fa3ef
Slynax <remlouplay3@gmail.com>
2025-01-27 16:30:11 +0100

    fix route

--- api/routes/setPlayer/index.js
+++ api/routes/setPlayer/index.js
@@ -1,8 +1,24 @@
+import Player from '../../../entities/player/index.js';
+
 export const setPlayer = async (req, res) => {
-    const updatedPlayer = await Player.findOneAndUpdate({}, playerData, { new: true });
-    if (!updatedPlayer) {
-        res.status(404).send({ message: 'Player data not updated' });
-        return;
+    try {
+        const playerData = req.body;
+
+        const updatedPlayer = await Player.findOneAndUpdate(
+            {},
+            {
+                pv: playerData.pv,
+                lvl: playerData.lvl,
+                inventory: playerData.inventory,
+                gold: playerData.gold,
+                dps: playerData.dps
+            },
+            { new: true, upsert: true, setDefaultsOnInsert: true }
+        );
+
+
+        res.status(200).send({ message: 'Player data updated', player: updatedPlayer });
+    } catch (error) {
+        res.status(500).send({ message: 'Erreur lors de la mise à jour du joueur.' });
     }
-    res.status(200).send({ message: 'Player data updated'});
-}
+};
