e92630d21ec57b582716ee05afab58dba307e6db
Crackvignoule <kiki.kalagan@gmail.com>
2025-01-28 19:09:32 +0100

    add RabbitMQ integration and update environment configuration

--- index.js
+++ index.js
@@ -5,21 +5,22 @@ import PublicEndpoints from './api/index.js';
 import { createServer } from 'http';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import {initWebSocket} from "./webSocket/index.js";
-const app = express();
-const port = 3002;
+import amqp from 'amqplib';
+import { initWebSocket } from "./webSocket/index.js";
 
 dotenv.config();
 
+const app = express();
+const port = process.env.PORT || 3002;
+
 app.use(express.json());
-
 app.use(cors({}));
-
 app.use(PublicEndpoints);
+
 const server = createServer(app);
 initWebSocket(server);
 
-mongoose.connect(process.env.MONGO_URL , {
+mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
@@ -35,8 +36,50 @@ mongoose.connect(process.env.MONGO_URL , {
         }
 
         server.listen(port, () => {
-            console.log(`Serveur actif sur http://localhost:${port}`);
-            console.log(`WebSocket actif sur ws://localhost:${port}`);
+            console.log(`Server running at http://localhost:${port}`);
+            console.log(`WebSocket running at ws://localhost:${port}`);
         });
+
+        await setupRabbitMQConsumers();
+
     })
     .catch(err => console.error('MongoDB connection failed:', err));
+
+async function setupRabbitMQConsumers() {
+    try {
+        const connection = await amqp.connect(`amqp://${process.env.RABBITMQ_HOST}`);
+        const channel = await connection.createChannel();
+
+        async function setupConsumer(playerId) {
+            const queue = playerId;
+
+            await channel.assertQueue(queue, {
+                durable: true
+            });
+
+            console.log(`Waiting for messages in queue: ${queue}`);
+
+            channel.consume(queue, async (msg) => {
+                if (msg !== null) {
+                    const messageContent = JSON.parse(msg.content.toString());
+                    console.log(`Received message: ${JSON.stringify(messageContent)}`);
+
+                    const playerId = messageContent.player._id;
+                    const updatedData = messageContent.player;
+
+                    await Player.findByIdAndUpdate(playerId, updatedData);
+
+                    channel.ack(msg);
+                }
+            }, {
+                noAck: false
+            });
+        }
+
+        const players = await Player.find({}, '_id');
+        players.forEach(player => setupConsumer(player._id.toString()));
+
+    } catch (err) {
+        console.error('RabbitMQ connection failed:', err);
+    }
+}
